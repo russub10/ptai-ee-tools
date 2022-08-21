0b08f49aa6a465b7707979ae87ac1c6ccc1a39f1
russub10 <38992943+russub10@users.noreply.github.com>
2022-08-21 16:11:42 +0300

    Add files via upload

---
+++ ptai-data-structures/src/main/java/com/ptsecurity/appsec/ai/ee/scan/result/ScanBrief.java
@@ -0,0 +1,201 @@
+package com.ptsecurity.appsec.ai.ee.scan.result;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ptsecurity.appsec.ai.ee.scan.settings.Policy;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+/**
+ * Class that stores top-level information about completed AST job. That
+ * information includes AST settings, policy assessment result and very
+ * short statistic about scan duration, number of scanned / skipped
+ * files / urls etc. This class have two descendants: ScanBriefDetailed
+ * and even more detailed ScanResult
+ */
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ScanBrief {
+    public enum ApiVersion {
+        V36, V40, V41
+    }
+
+    @NonNull
+    @Getter
+    @Setter
+    @JsonProperty
+    @Builder.Default
+    protected ApiVersion apiVersion = ApiVersion.V36;
+
+    @NonNull
+    @Getter
+    @Setter
+    @JsonProperty
+    protected String ptaiServerUrl;
+
+    @NonNull
+    @Getter
+    @Setter
+    @JsonProperty
+    protected String ptaiServerVersion;
+
+    @NonNull
+    @Getter
+    @Setter
+    @JsonProperty
+    protected String ptaiAgentVersion;
+
+    @NonNull
+    @Getter
+    @Setter
+    @JsonProperty
+    protected UUID id;
+
+    @NonNull
+    @Getter
+    @Setter
+    @JsonProperty
+    protected UUID projectId;
+
+    @NonNull
+    @Getter
+    @Setter
+    @JsonProperty
+    protected String projectName;
+
+    @Getter
+    @Setter
+    @JsonProperty
+    @Builder.Default
+    protected Boolean useAsyncScan = false;
+
+    @Getter
+    @Setter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ScanSettings {
+        @NonNull
+        @JsonProperty
+        protected UUID id;
+
+        public enum Engine {
+            AI, PM, TAINT, DC, FINGERPRINT, CONFIGURATION, BLACKBOX
+        }
+
+        @Builder.Default
+        @JsonProperty
+        protected final Set<Engine> engines = new HashSet<>();
+
+        @JsonProperty
+        protected Boolean unpackUserPackages;
+
+        @JsonProperty
+        protected Boolean downloadDependencies;
+
+        @JsonProperty
+        protected Boolean usePublicAnalysisMethod;
+
+        @JsonProperty
+        protected Boolean useEntryAnalysisPoint;
+
+        @RequiredArgsConstructor
+        public enum Language {
+            PHP("PHP"),
+            JAVA("Java"),
+            CSHARP("CSharp"),
+            VB("VB"),
+            JAVASCRIPT("JavaScript"),
+            GO("Go"),
+            CPP("CPlusPlus"),
+            PYTHON("Python"),
+            SQL("SQL"),
+            OBJECTIVEC("ObjectiveC"),
+            SWIFT("Swift"), KOTLIN("Kotlin");
+
+            public static Language fromString(@NonNull final String value) {
+                for (Language language : Language.values())
+                    if (language.value.equalsIgnoreCase(value)) return language;
+                throw new IllegalArgumentException("No enum value " + Language.class.getCanonicalName() + "." + value);
+            }
+
+            @NonNull
+            private final String value;
+        }
+
+        @JsonProperty
+        protected Language language;
+
+        @JsonProperty
+        protected String url;
+
+        @JsonProperty
+        protected Boolean useIncrementalScan;
+
+        @JsonProperty
+        protected Boolean autocheckAfterScan;
+
+        @JsonProperty
+        protected String customParameters;
+
+        @JsonProperty
+        protected String javaParameters;
+    }
+
+    @NonNull
+    @Getter
+    @Setter
+    @JsonProperty
+    protected ScanSettings scanSettings;
+
+    @Getter
+    @Setter
+    @NonNull
+    @Builder.Default
+    @JsonProperty
+    protected Policy.State policyState = Policy.State.NONE;
+
+    @Getter
+    @Setter
+    @SuperBuilder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @ToString
+    public static class Statistics {
+        /**
+         * Scan execution date / time. Can't use Java 8 ZonedDateTime, Instant etc. as Jenkins
+         * complaints "Refusing to marshal java.time.Instant for security reasons;
+         * see https://jenkins.io/redirect/class-filter/"
+         */
+        @NonNull
+        @JsonProperty
+        protected String scanDateIso8601;
+
+        @NonNull
+        @JsonProperty
+        protected String scanDurationIso8601;
+
+        protected int totalFileCount;
+        protected int totalUrlCount;
+        protected int scannedFileCount;
+        protected int scannedUrlCount;
+    }
+
+    @Getter
+    @Setter
+    protected Statistics statistics;
+
+    public enum State {
+        UNKNOWN, DONE, FAILED, ABORTED
+    }
+
+    @Getter
+    @Setter
+    @NonNull
+    @Builder.Default
+    protected ScanBrief.State state = ScanBrief.State.UNKNOWN;
+}
---
+++ ptai-data-structures/src/main/java/com/ptsecurity/appsec/ai/ee/scan/result/issue/types/BaseIssue.java
@@ -0,0 +1,165 @@
+package com.ptsecurity.appsec.ai.ee.scan.result.issue.types;
+
+import com.fasterxml.jackson.annotation.*;
+import com.ptsecurity.appsec.ai.ee.scan.reports.Reports;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+/**
+ * Common base parent class for all issues. As it is base it does
+ * contain only fields that are not specific
+ */
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.PROPERTY,
+        property = "class")
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = BlackBoxIssue.class, name = "BLACKBOX"),
+        @JsonSubTypes.Type(value = ConfigurationIssue.class, name = "CONFIGURATION"),
+        @JsonSubTypes.Type(value = ScaIssue.class, name = "SCA"),
+        @JsonSubTypes.Type(value = UnknownIssue.class, name = "UNKNOWN"),
+        @JsonSubTypes.Type(value = VulnerabilityIssue.class, name = "VULNERABILITY"),
+        @JsonSubTypes.Type(value = WeaknessIssue.class, name = "WEAKNESS"),
+        @JsonSubTypes.Type(value = YaraMatchIssue.class, name = "YARAMATCH")
+})
+public abstract class BaseIssue {
+    public static Map<Class<? extends BaseIssue>, Type> TYPES = new HashMap<>();
+
+    static {
+        TYPES.put(UnknownIssue.class, Type.UNKNOWN);
+        TYPES.put(BlackBoxIssue.class, Type.BLACKBOX);
+        TYPES.put(ConfigurationIssue.class, Type.CONFIGURATION);
+        TYPES.put(ScaIssue.class, Type.SCA);
+        TYPES.put(WeaknessIssue.class, Type.WEAKNESS);
+        TYPES.put(VulnerabilityIssue.class, Type.VULNERABILITY);
+        TYPES.put(YaraMatchIssue.class, Type.YARAMATCH);
+    }
+
+    /**
+     * Unique issue identifier
+     */
+    @JsonProperty("id")
+    protected String id;
+
+    /**
+     * Issue group identifier. Null if issue doesn't belong to group
+     */
+    @JsonProperty("groupId")
+    protected String groupId;
+
+    /**
+     * Unique issue type identifier
+     */
+    @JsonProperty("typeId")
+    protected String typeId;
+
+    /**
+     * Scan result this issue belongs to
+     * TODO: Check if we may get rid of this
+     */
+    @NonNull
+    @JsonProperty("scanResultId")
+    protected UUID scanResultId;
+
+    public enum Type {
+        VULNERABILITY, WEAKNESS, SCA, CONFIGURATION, BLACKBOX, YARAMATCH, UNKNOWN
+    }
+
+    /**
+     * Issue type: vulnerability, weakness, SCA, DAST etc.
+     */
+    @JsonIgnore
+    public Type getClazz() {
+        return TYPES.get(getClass());
+    }
+
+    @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+    public enum Level {
+        NONE(0),
+        POTENTIAL(1),
+        LOW(2),
+        MEDIUM(3),
+        HIGH(4);
+
+        @Getter
+        private final int value;
+    }
+
+    /**
+     * Issue severity level
+     */
+    @JsonProperty("level")
+    protected Level level;
+
+    /**
+     * True if issue marked as favorite in UI
+     */
+    @JsonProperty("isFavorite")
+    protected Boolean favorite;
+
+    /**
+     * True if issue is a suspected vulnerability i.e. PT AI not
+     * sure if it can be exploited
+     */
+    @JsonProperty("isSuspected")
+    protected Boolean suspected;
+
+    /**
+     * True if issue marked with suppress comment in source code
+     */
+    @JsonProperty("isSuppressed")
+    protected Boolean suppressed;
+
+    /**
+     * Issue approval state. This state persisted between scans: if vulnerability
+     * was marked as approved after last scan but developer haven't fixed it then
+     * approval state will be automatically assigned to issue in a new scan results
+     */
+    public enum ApprovalState {
+        /**
+         * No approval state defined
+         */
+        NONE,
+        /**
+         * Issue approved
+         */
+        APPROVAL,
+        /**
+         * Issue declined
+         */
+        DISCARD,
+        NOT_EXIST,
+        /**
+         * Issue approved during auto-check stage
+         */
+        AUTO_APPROVAL
+    }
+
+    /**
+     * Issue approval state @see IssueApprovalState
+     */
+    @JsonProperty("approvalState")
+    protected ApprovalState approvalState;
+
+    @JsonProperty("newInScanResultId")
+    protected UUID newInScanResultId;
+
+    @JsonProperty("oldInScanResultId")
+    protected UUID oldInScanResultId;
+
+    /**
+     * Unique vulnerability type identifier in the CWE classifier
+     */
+    @JsonProperty("cweId")
+    protected List<String> cweId;
+}
